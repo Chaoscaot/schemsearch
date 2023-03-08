5c12ce12024439c4a1293fc916dcf29cddbecb96
Chaoscaot <chaoscaot@zohomail.eu>
2023-03-08 22:18:40 +0100

    Add AGPL Headers

--- schemsearch-cli/src/main.rs
+++ schemsearch-cli/src/main.rs
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023  Chaoscaot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 mod types;
 
 use std::fs::File;
@@ -6,12 +23,12 @@ use clap::{command, Arg, ArgAction, Command};
 use schemsearch_files::Schematic;
 use std::path::Path;
 use clap::error::ErrorKind;
+use schemsearch_lib::{search, SearchBehavior};
+use crate::types::{PathSchematicSupplier, SchematicSupplierType};
 #[cfg(feature = "sql")]
 use futures::executor::block_on;
-use schemsearch_lib::{search, SearchBehavior};
 #[cfg(feature = "sql")]
 use schemsearch_sql::load_all_schematics;
-use crate::types::{PathSchematicSupplier, SchematicSupplierType};
 #[cfg(feature = "sql")]
 use crate::types::SqlSchematicSupplier;
 
