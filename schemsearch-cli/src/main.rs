8befbf4c7f2e1189788165b4b1bdad733108afa9
Chaoscaot <chaos@chaoscaot.de>
2025-04-09 18:32:16 +0200

    Add CPU matching option and improve default behavior handling

--- schemsearch-cli/src/main.rs
+++ schemsearch-cli/src/main.rs
@@ -169,7 +169,7 @@ fn main() {
                 .value_parser(|s: &str| s.parse::<usize>().map_err(|e| e.to_string())),
         )
         .arg(
-            Arg::new("cpu")
+            Arg::new("force-cpu")
                 .help("Force CPU Matching, skip OpenCL checks")
                 .short('c')
                 .long("force-cpu")
@@ -221,7 +221,7 @@ fn main() {
         ignore_entities: matches.get_flag("ignore-entities"),
         threshold: *matches.get_one::<f32>("threshold").expect("Couldn't get threshold"),
         invalid_nbt: matches.get_flag("invalid-nbt"),
-        use_cpu: matches.get_flag("use-cpu"),
+        use_cpu: matches.get_flag("force-cpu"),
     };
 
     let pattern = match matches.get_one::<String>("pattern") {
